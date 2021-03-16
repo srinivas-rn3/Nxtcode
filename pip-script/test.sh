60769d44f7763c657dc16acd58b853cce4467f68
Srinivas <46322090+srinivas-rn3@users.noreply.github.com>
2021-03-16 12:02:44 +0530

    Create test.sh
    test script

---
+++ pip-script/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SERVERIP=104.210.8.183
+##NOTIFYEMAIL=test@example.com
+
+ping -c 3 $SERVERIP > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+   # Use your favorite mailer here:
+   #mailx -s "Server $SERVERIP is down" -t "$NOTIFYEMAIL" < /dev/null 
+   echo "Server Online !!!!!!!!!"
+fi
