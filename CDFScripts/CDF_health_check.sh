91375875fdff6f1a0678fab35a87593a8da0dfd9
Srinivas <46322090+srinivas-rn3@users.noreply.github.com>
2021-05-17 21:56:05 +0530

    Update CDF_health_check.sh

--- CDFScripts/CDF_health_check.sh
+++ CDFScripts/CDF_health_check.sh
@@ -20,20 +20,20 @@ MOUNT_LOC="/var/vols/itom"
 
 ###Node Status ###
 
-echo "----------------------Nodes Status ----------------------------"
+echo "---------------------- --------------- Nodes Status --------------------------------------------"
 NODES_STATUS=`kubectl get no`
 printf "%-30s|%-18s|%-20s|%-20s|%-20s\n" $NODES_STATUS
 echo -e "\n"
 ### Nodes CPU AND Memory Status #####
 
-echo "------------------------- K8's Nodes Memory and CPU Consumption ------------------------------"
+echo "------------------------- K8's Nodes Memory and CPU Consumption ----------------------------------"
 NODES_CON=`kubectl top no` 
 printf "%-30s|%-18s|%-20s|%-20s|%-20s\n" $NODES_CON 
 echo -e "\n"
 ###### Check for PODs Crash Loop Back Status####
 echo "----------------- Crashloop back pods Status ------------------------------"
 if [[ ! -z `kubectl get pods --all-namespaces | grep CrashLoopBackOff` ]] ; then
-    echo " Crashloopback pods"
+    #echo " Crashloopback pods"
     result1=`kubectl get po --all-namespaces | grep CrashLoopBackOff`
     echo $result1
 else
@@ -43,7 +43,7 @@ echo -e "\n"
 ########## Check for PODs in Error State ######
 echo "----------------- Error pods Status ------------------------------"
 if  [[ ! -z `kubectl get pods --all-namespaces | grep Error` ]] ; then
-    echo "fetching Error pods"
+    #echo "fetching Error pods"
     result2=`kubectl get po --all-namespaces | grep Error`
     echo $result2
 else
@@ -51,7 +51,7 @@ else
 fi
 echo -e "\n"
 ########### Checking Mount Status ######
-echo "----------------- Mount Point Status ------------------------------"
+echo "------------------------ Mount Point Status ------------------------------"
 if mount | grep $MOUNT_LOC > /dev/null; then
     echo "$MOUNT_LOC Moutned!!!"
 else
