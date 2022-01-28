14b3e67af06c64af08045f7ca519bd7869d9dd37
Srinivas R.N <srinivas.rn2@microfocus.com>
2022-01-28 19:31:18 +0530

    class example

--- IT AUTOMATION/My_code9.py
+++ IT AUTOMATION/My_code9.py
@@ -1,3 +1,4 @@
+'''
 # Define higher order function
 def fun1(foo):
     result = foo('welcome!!!')
@@ -14,3 +15,22 @@ print (str1)
 
 str2 = fun1(fun3)
 print(str2)
+'''
+'''
+class check:
+    def __init__(self):
+        print("adddress of self :", id(self))
+'''
+class car():
+    def __init__(self,model,color):
+        self.model = model
+        self.color = color
+    def show(self):
+        print("Model", self.model)
+        print("color", self.color)
+        
+audi = car("audi a4", "black")
+ferrari = car("ferrari 488", "red")    
+
+audi.show()
+ferrari.show()
