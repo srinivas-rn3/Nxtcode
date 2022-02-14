771715e2e2d242bdd1b93d39408fe76d0e12ecf1
Srinivas R.N <srinivas.rn2@microfocus.com>
2022-02-14 15:30:36 +0530

    test py with json

--- IT AUTOMATION/.vscode/search_paterrna.py
+++ IT AUTOMATION/.vscode/search_paterrna.py
@@ -1,10 +1,12 @@
 import json
 import sys
 import pickle
+import pandas as pd
  #json load file location
-json_file = 'C://Users//rnsri//OneDrive - Micro Focus//DXC Support Project//ETL//Service_node.json'
+#json_file = 'C://Users//rnsri//OneDrive - Micro Focus//DXC Support Project//ETL//Service_node.json'
+json_file = 'C://Users//rnsri//OneDrive - Micro Focus//DXC Support Project//service_node_CI_Details.json'
 #converted csv file location
-output_csv = 'C://Users//rnsri//OneDrive - Micro Focus//DXC Support Project//ETL//test3.csv'
+output_csv = 'C://Users//rnsri//OneDrive - Micro Focus//DXC Support Project//ETL//test333.csv'
 with open(json_file) as f:
     data = json.load(f)
     #print(data)
@@ -18,11 +20,24 @@ for i in data["relations"]:
         #print(s) 
         t = i["end2Id"]
         #print(t)
+        u = n["globalId"]
+        print(u)
         if s == n["ucmdbId"]:
           print(n["ucmdbId"])
           list1.append(t)
-          list2.append(n["globalId"])
+          #list2.append(n["globalId"])
+          list2.append(u)
         else:
           print("not matching")
-print(list1)
-print(list2)
+print("ucmdb ID:" ,list1)
+print("Global ID:",list2)
+
+'''
+df = pd.DataFrame({'Device_Global ID ':list1,'ServiceComp_Global ID':list2})
+
+print(df)
+
+#df1 = df.drop_duplicates(subset=None, keep='first', inplace=False, ignore_index=False)
+
+df.to_csv(output_csv,index=False)
+'''
