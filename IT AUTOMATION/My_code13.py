054da123d1cf077032d6f343177caa159dfb784e
Srinivas R.N <srinivas.rn2@microfocus.com>
2022-03-12 22:22:24 +0530

    my learning

---
+++ IT AUTOMATION/My_code13.py
@@ -0,0 +1,37 @@
+'''
+'''
+#string1 = '''I'm the one who owns the time!!!'''
+#print(string1)
+#string2 = '''
+#           I'm the one who ones the light
+#          I'm creator of the Big Bang!!!
+#           '''
+#print(string2)
+'''
+'''
+
+'''
+Accessing characters in Python
+In Python, individual characters of a String can be accessed by using the method of Indexing. 
+Indexing allows negative address references to access characters from the back of the String,
+e.g. -1 refers to the last character, -2 refers to the second last character, and so on. 
+'''
+'''
+string1 = "Man who Eats Time"
+print("Initial string is : ")
+print(string1)
+print("first index is :")
+print(string1[0])
+'''
+'''
+String Slicing
+To access a range of characters in the String, the method of slicing is used. 
+Slicing in a String is done by using a Slicing operator (colon). 
+'''
+string1 = "My Dog having black spots"
+print(string1)
+print("slicing")
+print(string1[2:6])
+print(string1[-4])
+print(string1[-4:3])
+##https://www.geeksforgeeks.org/python-strings/
