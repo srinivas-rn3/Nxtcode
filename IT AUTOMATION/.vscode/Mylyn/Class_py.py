c22d937063b496621dbe2ab980cea9fe5a44e94e
Srinivas R.N <srinivas.rn2@microfocus.com>
2022-09-28 14:21:17 +0530

    My learning

--- IT AUTOMATION/.vscode/Mylyn/Class_py.py
+++ IT AUTOMATION/.vscode/Mylyn/Class_py.py
@@ -1,3 +1,4 @@
+'''
 class Human:
     def __init__(self,n,o):
       self.name = n
@@ -17,3 +18,32 @@ tom.speaks()
 maria = Human("Maria Sharpova", "tennis player")
 maria.do_work()
 maria.speaks()
+'''
+
+class calc:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def add(self):
+        return self.x + self.y
+    def sub(self):
+        return self.x - self.y
+    def mul(self):
+        return self.x * self.y
+    def div(self):
+        try:
+            return self.x / self.y
+        except ZeroDivisionError as e:
+            print("Zero by error\n")
+            return None
+
+x1 = int(input("enter the no. : "))
+x2 = int(input("Enter the No. : "))
+print("\n")
+
+z = calc(x1,x2)
+
+print("addition value is: ",z.add())
+print("Subtraction value is: ",z.sub())
+print("Multilcation vlaue is: ", z.mul())
+print("Divion value is: ", z.div())
