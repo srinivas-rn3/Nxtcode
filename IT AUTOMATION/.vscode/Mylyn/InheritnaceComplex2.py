30401fc3af65d5c2ca0b79dcb03a84c6cc9c2775
Srinivas R.N <srinivas.rn2@microfocus.com>
2022-10-31 14:35:19 +0530

    My Code

---
+++ IT AUTOMATION/.vscode/Mylyn/InheritnaceComplex2.py
@@ -0,0 +1,102 @@
+#https://www.youtube.com/watch?v=RSl87lqOXDE
+#Inheritance - Creating Subclasses
+'''
+class Employee:
+    raise_amt = 1.04
+    def __init__(self, first , last ,pay):
+        self.first = first 
+        self.last = last 
+        self.email = first+'.'+last+'@next-think.com'
+        self.pay = pay
+    def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+
+    def apply_riase(self):
+        self.pay = int(self.pay  *self.raise_amt)
+
+class Developer(Employee):
+    raise_amt = 1.10
+    def __init__(self,first,last,pay,prog_lang):
+        super().__init__(first,last,pay)
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+    def __init__(self,first,last,pay,employees=None):
+        super().__init__(first,last,pay)
+        if employees  is  None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self,emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self,emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+    def print_emp(self):
+        for emp in self.employees:
+            print('-->',emp.fullname())
+    
+dev1 = Developer('Srini','RN',89000,'Python')
+dev2 = Developer('Marnie','Sapparo',78888,'Julia')
+#print(dev1.prog_lang)
+#print(dev2.prog_lang)
+mgr_1 = Manager('Bella','Hadid',100000,[dev1])
+
+print(mgr_1.email)
+mgr_1.print_emp()
+mgr_1.add_emp(dev2)
+mgr_1.print_emp()
+mgr_1.remove_emp(dev2)
+print('after removal',end='\n')
+mgr_1.print_emp()
+#print(dev1.fullname())
+#print(help(Developer))
+#print(dev1.pay)
+#dev1.apply_riase()
+#print(dev1.pay)
+
+print(isinstance(mgr_1,Employee))
+print(issubclass(Developer,Manager))
+'''
+#Python OOP Tutorial 5: Special (Magic/Dunder) Methods
+#https://www.youtube.com/watch?v=3ohzBxoFHAY
+
+class Employee:
+    raise_amt = 1.04
+    def __init__(self, first , last ,pay):
+        self.first = first 
+        self.last = last 
+        self.email = first+'.'+last+'@next-think.com'
+        self.pay = pay
+    def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+
+    def apply_riase(self):
+        self.pay = int(self.pay  *self.raise_amt)
+    def __repr__(self) -> str:
+        return "Employee('{}','{}','{}')".format(self.first,self.last,self.pay)
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(),self.email)
+    def __add__(self,other):
+        return self.pay + other.pay
+    def __len__(self):
+        return len(self.fullname())
+
+dev1 = Employee('Srini','RN',89000)
+dev2 = Employee('Marnie','Sapparo',78888)
+
+#print(dev1)
+
+##rint(repr(dev1))
+#print(str(dev1))
+#print(dev1.__repr__())
+#print(dev1.__str__())
+
+#print(int.__add__(1,3))
+#print(int.__str__('a'+'c'))
+print(dev1 + dev2)
+print(len(dev1))
+#print('test'.__len__())
--- IT AUTOMATION/.vscode/Mylyn/class_inher_py2.py
+++ IT AUTOMATION/.vscode/Mylyn/class_inher_py2.py
@@ -59,3 +59,148 @@ print(Employee.raise_amnt)
 '''
 #classmethods and staticmethods
 #https://www.youtube.com/watch?v=rq8cL2XMM5M
+'''
+def outer_func(msg):
+    message = msg
+
+    def inner_func():
+
+       print(message)
+    return inner_func
+my_fucn1 = outer_func('Hi')
+my_fucn2 = outer_func('Fuck You')
+my_fucn1(); my_fucn2()
+'''
+'''
+def decorator_functions(message):
+    def wrapper_function():
+        print(message)
+    return wrapper_function
+'''
+'''
+def decorator_function(orginal_function):
+    def wrapper_function(*args,**kwargs):
+        print("Wrapper fucntion is executed first {}". format(orginal_function.__name__))
+        return orginal_function(*args,**kwargs)
+    return wrapper_function
+
+@decorator_function
+def display():
+    print('display function ran!!')
+
+@decorator_function
+def display_info(name, age):
+    print('Display info with two arguments ({},{})'.format(name, age))
+
+#def1 = decorator_function(display)
+
+display_info('Sri', 30)
+display()
+'''
+'''
+class decorator_class(object):
+    def __init__(self,orginal_function):
+        self.orginal_function = orginal_function
+    def __call__(self, *args,**kwargs):
+         print("class fucntion is executed first {}". format(self.orginal_function.__name__))
+         return self.orginal_function(*args,**kwargs)
+@decorator_class
+def display():
+    print('display function ran!!')
+
+@decorator_class
+def display_info(name, age):
+    print('Display info with two arguments ({},{})'.format(name, age))
+
+display()
+display_info('Sri', 30)
+'''
+'''
+import time
+from functools import wraps
+def my_logger(orgi_func):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(orgi_func.__name__),level=logging.INFO)
+    @wraps(orgi_func)
+    def wrapper(*args, **kwargs):
+        logging.info(
+            'Ran with args: {} , and kwargs:{}'.format(args,kwargs)
+        )
+        return orgi_func(*args, **kwargs)
+    return wrapper
+
+def my_timer(orgi_func):
+    import time
+    @wraps(orgi_func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result =  orgi_func(*args , **kwargs) 
+        t2 = time.time() - t1
+        print('{} ran in: {} sec'. format(orgi_func.__name__, t2))
+        return result 
+    return wrapper
+@my_logger
+@my_timer
+def display_info(name, age):
+    time.sleep(5)
+    print('Display info with two arguments ({}, {})'.format(name, age))
+dis = my_timer(display_info)
+print(dis.__name__)
+display_info("kylie", 30)
+'''
+
+class Employee:
+    no_of_emps = 0
+    raise_amt = 1.04
+    def __init__(self,first,last,pay):
+        self.first = first
+        self.pay = pay 
+        self.last = last 
+        self.email = first + '.' + last + '@comp.com'
+
+        Employee.no_of_emps += 1
+    def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+        return self.pay
+    @classmethod
+    def set_raise_amount(cls,amount):
+        cls.raise_amt = amount
+    @classmethod
+    def from_string(cls,emp_):
+        first,last,pay = emp_.split('-')
+        return cls(first, last,pay)
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+'''
+emp1 = Employee('Srini', 'RN',100000)
+emp2 = Employee('Marnie', 'saperro', 900000)
+
+Employee.set_raise_amount(1.08)
+
+print(emp2.apply_raise())
+print(Employee.raise_amt)
+print(emp1.raise_amt)
+print(emp2.raise_amt)
+'''
+'''
+emp_1 = 'John-Dee-70000'
+emp_2 = 'Srini-RN-70000'
+emp_3 =  'Marnie-Sapparo-80000'
+
+#first,last,pay = emp_1.split('-')
+#new_emp_1 = Employee(first,last,pay)
+new_emp_1 = Employee.from_string(emp_1)
+print(new_emp_1.email)
+print(new_emp_1.pay)
+'''
+import datetime
+my_date = datetime.date(2022,10,30)
+
+print(Employee.is_workday(my_date))
