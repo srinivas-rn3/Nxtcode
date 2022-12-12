4332ee83a16c9b7ef7ea8689779362b4d89841a7
Srinivas R.N <srinivas.rn2@microfocus.com>
2022-12-12 20:06:02 +0530

    My learning

--- IT AUTOMATION/.vscode/Mylyn/pandas6.py
+++ IT AUTOMATION/.vscode/Mylyn/pandas6.py
@@ -1,12 +1,15 @@
 #https://www.youtube.com/watch?v=HQ6XO9eT-fc&list=PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS&index=6
 #Python Pandas Tutorial (Part 6): Add/Remove Rows and Columns From DataFrames
+'''
 people = {
-    "first": ['Srini','Marine','Jane','Kim'],
-    "last": ['RN','Chang','Doe','Doe'],
-    "email": ['srinivas.rn@xxx.com','marnie.change@xxx.com','jane.doe@xxx.com','kim.doe@xxx.com']
+    "first": ['Srini','Marine','Jane','Kim','Adam'],
+    "last": ['RN','Chang','Doe','Doe','Adrian'],
+    "email": ['srinivas.rn@xxx.com','marnie.change@xxx.com','jane.doe@xxx.com','kim.doe@xxx.com','adam.adrian@xxx.com']
 }
-import pandas as pd 
+'''
+import pandas as pd
 
+'''
 df1 = pd.DataFrame(people)
 #print(df1)
 df1['full_name'] = df1['first']+ ' '+df1['last']
@@ -26,7 +29,7 @@ heros = {
 }
 df2 = pd.DataFrame(heros)
 #print(df2)
-#merge tow data frame
+#merge two data frame
 df1 = df1.append(df2,ignore_index=True)
 print(df1)
 df1 = df1.drop(index=4)#to drop Index
@@ -38,5 +41,34 @@ print(df1)
 filt = df1['last'] == 'Doe'
 df1 = df1.drop(index=df1[filt].index)
 print(df1)
+'''
 #Python Pandas Tutorial Sorting Data
 #https://www.youtube.com/watch?v=T11QYVfZoD0&list=PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS&index=7
+#df = pd.DataFrame(people)
+#print(df.sort_values(by='last',ascending=False))
+#print(df.sort_values(by=['last','first'],ascending=False))
+#asending in specfic cloumn
+#df.sort_values(by=['last','first'],ascending=[False,True],inplace=True)
+#print(df)
+#df.sort_index()#To sort by index
+#print(df)
+#sort single series
+#print(df['last'].sort_values())
+#print(df)
+#stackoverflow Data
+pd_schema = pd.read_csv(r'C:/Users/rnsri/Stackoverflow/survey_results_schema.csv')
+pd_public = pd.read_csv(r'C:/Users/rnsri/Stackoverflow/survey_results_public.csv')
+
+pd.set_option('display.max_columns',85)
+pd.set_option('display.max_rows',85)
+df_pub = pd.DataFrame(pd_public)
+df_schema = pd.DataFrame(pd_schema)
+
+#df_pub.sort_values(by=['Country','ConvertedCompYearly'],ascending=[True,False],inplace=True)
+#print(df_pub[['Country','ConvertedCompYearly']].head(50))
+#to sort biggest numbers in given columns
+#print(df_pub['ConvertedCompYearly'].nlargest(50))
+#For Entire DataFrame to apply nlargest
+#print(df_pub.nlargest(10,'ConvertedCompYearly'))
+#For smallest valeus 
+print(df_pub.nsmallest(10,'ConvertedCompYearly'))
---
+++ IT AUTOMATION/.vscode/Mylyn/pandas7.py
@@ -0,0 +1,2 @@
+#Python Pandas Tutorial (Part 8): Grouping and Aggregating - Analyzing and Exploring Your Data
+#https://www.youtube.com/watch?v=txMdrV1Ut64&list=PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS&index=8
