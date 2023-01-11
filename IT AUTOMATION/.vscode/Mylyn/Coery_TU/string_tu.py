700b065bd89b55dc9cc6fb7c26d9b6e79e0e21f2
Srinivas RN <srinivas.rn3@gmail.com>
2023-01-11 11:14:53 +0530

    String code python

--- IT AUTOMATION/.vscode/Mylyn/Coery_TU/string_tu.py
+++ IT AUTOMATION/.vscode/Mylyn/Coery_TU/string_tu.py
@@ -1,1 +1,14 @@
 #https://www.youtube.com/watch?v=k9TUPpGqYTo&ab_channel=CoreySchafer
+#string
+#message = "Hole's is big"
+msg = '''Hi I'm black hole.
+I can eat your planet.'''
+#print(len(msg))
+#print(msg[6:])
+#print(msg.lower())
+#print(msg.upper())
+#print(msg.count('o'))
+#print(msg.find('planet'))
+msg = msg.replace('planet','Universe')
+print(msg)
+#12:49
