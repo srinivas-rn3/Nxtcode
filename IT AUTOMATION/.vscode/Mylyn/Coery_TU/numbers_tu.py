9d441dcf8029472ea7fa779617e7f63ce929f3d1
Srinivas RN <srinivas.rn3@gmail.com>
2023-01-12 21:12:38 +0530

    my learnings

---
+++ IT AUTOMATION/.vscode/Mylyn/Coery_TU/numbers_tu.py
@@ -0,0 +1,1 @@
+#https://www.youtube.com/watch?v=khKv-8q7YmY&ab_channel=CoreySchafer
--- IT AUTOMATION/.vscode/Mylyn/Coery_TU/string_tu.py
+++ IT AUTOMATION/.vscode/Mylyn/Coery_TU/string_tu.py
@@ -1,14 +1,25 @@
 #https://www.youtube.com/watch?v=k9TUPpGqYTo&ab_channel=CoreySchafer
 #string
 #message = "Hole's is big"
-msg = '''Hi I'm black hole.
-I can eat your planet.'''
+#msg = '''Hi I'm black hole.
+#I can eat your planet.'''
 #print(len(msg))
 #print(msg[6:])
 #print(msg.lower())
 #print(msg.upper())
 #print(msg.count('o'))
 #print(msg.find('planet'))
-msg = msg.replace('planet','Universe')
-print(msg)
+#msg = msg.replace('planet','Universe')
+#print(msg)
 #12:49
+#greetings = 'Hello'
+#name = 'Srini'
+#message = greetings +','+ name + '.Welcome!'
+#message = '{},{}.Welcome!!!'.format(greetings,name)
+#message = f'{greetings.lower()},{name.upper()}.Welcome!!!'
+#print(message)
+#print(dir(name))
+#print(help(str))
+#print(help(str.upper))
+#Python Tutorial: String Formatting - Advanced Operations for Dicts, Lists, Numbers, and Dates
+#https://www.youtube.com/watch?v=khKv-8q7YmY&ab_channel=CoreySchafer
